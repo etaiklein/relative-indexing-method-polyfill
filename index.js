deea2a5bf970500e35b071c05635bc53a651c42b
Tai Klein <etai@hioscar.com>
2021-02-23 21:20:46 -0500

    iife

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-function at(n) {
-	// ToInteger() abstract op
-	n = Math.trunc(n) || 0;
-	// Allow negative indexing from the end
-	if(n < 0) n += this.length;
-	// OOB access is guaranteed to return undefined
-	if(n < 0 || n >= this.length) return undefined;
-	// Otherwise, this is just normal property access
-	return this[n];
-}
+(function () {
+  function at(n) {
+    // ToInteger() abstract op
+    n = Math.trunc(n) || 0;
+    // Allow negative indexing from the end
+    if (n < 0) n += this.length;
+    // OOB access is guaranteed to return undefined
+    if (n < 0 || n >= this.length) return undefined;
+    // Otherwise, this is just normal property access
+    return this[n];
+  }
 
-// Other TypedArray constructors omitted for brevity.
-for (let C of [Array, String, Uint8Array]) {
-    Object.defineProperty(C.prototype, "at",
-                          { value: at,
-                            writable: true,
-                            enumerable: false,
-                            configurable: true });
-}
+  // Other TypedArray constructors omitted for brevity.
+  for (let C of [Array, String, Uint8Array]) {
+    Object.defineProperty(C.prototype, "at", {
+      value: at,
+      writable: true,
+      enumerable: false,
+      configurable: true,
+    });
+  }
+})();
